e1e6e9a35da614a48b5359fc72ef27763f589bb2
MJ231 <jayymedeiros@gmail.com>
2021-07-10 16:07:57 -0700

    Abstract Class
    Coding Program.

---
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person //Create class
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName); //Display name
+        }
+    }
+}
---
+++ Basic_C#_Programs/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); //Instantiate employee object
+            employee.FirstName = "Sample"; //Display name
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine(); //End program
+        }
+    }
+}
