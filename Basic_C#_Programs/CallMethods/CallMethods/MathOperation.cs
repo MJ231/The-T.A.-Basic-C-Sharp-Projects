f5452f95d2c1937a91b3f2c50307a35e7bd8764e
MJ231 <jayymedeiros@gmail.com>
2021-07-10 22:38:59 -0700

    Calling Methods
    Coding Program.

---
+++ Basic_C#_Programs/CallMethods/CallMethods/MathOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethods
+{
+    class MathOperation //Create class
+    {
+        public static int Add(int numOne)
+        {
+            return numOne + 100; //int Add1 = num1 + 100;
+        }
+        public static int Mult (int numOne)
+        {
+            return numOne * 40; //int Mult1 = num1 * 40;
+        }
+        public static int Subtract (int numOne)
+        {
+            return numOne - 10; //int Subtract1 = num1 - 10;
+        }    
+    }
+}
---
+++ Basic_C#_Programs/CallMethods/CallMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using CallMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a Number and It'll Perform Three Math Functions:"); //User Input
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            
+            
+            Console.WriteLine(numOne + " + 100 = " + MathOperation.Add(numOne)); //Sum of two numbers
+            Console.WriteLine(numOne + " x 40 = " + MathOperation.Mult(numOne)); //Multiplication of two numbers
+            Console.WriteLine(numOne + " - 10 = " + MathOperation.Subtract(numOne)); //Subtraction of two numbers
+            Console.WriteLine("You are Awesome!");
+            Console.ReadLine(); //End program
+            
+        }
+    }
+}
