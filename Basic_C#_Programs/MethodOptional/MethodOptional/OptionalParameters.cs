1962951c5e0702abdaf1413786569668524c091e
MJ231 <jayymedeiros@gmail.com>
2021-07-11 02:24:22 -0700

    Method Optional
    Coding Program.

---
+++ Basic_C#_Programs/MethodOptional/MethodOptional/OptionalParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptional
+{
+    public class OptionalParameters //Class name
+    {
+        public int MathMethod(int numOne, string ans) //Method initiated
+        {
+            int result = 0; //Give integers an operation
+            if (ans == "")
+            {
+                result = numOne + 10;
+            }
+            else
+            {
+                int numTwo = Convert.ToInt32(ans);
+                result = numOne + numTwo + 10;
+            }
+            return result; //Returns results 
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodOptional/MethodOptional/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a Number Please."); //User input
+            int numOne = Convert.ToInt32(Console.ReadLine()); //Convert integer to a whole number
+            Console.WriteLine("Pick a Second Number if You Dare. Otherwise, just Hit Return."); //User input
+            string ans = Console.ReadLine(); //Add string
+
+            OptionalParameters FirOp = new OptionalParameters(); //Integrate other class
+
+            Console.WriteLine("Your Input(s) + 10 = " + FirOp.MathMethod(numOne, ans)); //Get results
+
+            Console.ReadLine(); //End program
+        }
+    }
+}
