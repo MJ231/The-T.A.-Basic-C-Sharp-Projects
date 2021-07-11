fc3e3429ac8f4c8ec6b5d39ba3f4ebd7423eb891
MJ231 <jayymedeiros@gmail.com>
2021-07-11 00:59:45 -0700

    Method Class Assignment
    Coding Program.

---
+++ Basic_C#_Programs/Methods2/Methods2/Parameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Parameters
+    {
+        public void voidMethod(int parameter1, int parameter2) //Void of two numbers
+        {
+            int number = parameter1 + 20;
+            Console.WriteLine("The Second Parameter is: " + parameter2 + "."); //Initiate results
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods2/Methods2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parameters paraMeters = new Parameters(); //Name parameters
+            int parameter1 = 20;
+            int parameter2 = 40;
+            paraMeters.voidMethod(parameter1, parameter2);
+            Console.ReadLine(); //End program
+        }
+    }
+}
