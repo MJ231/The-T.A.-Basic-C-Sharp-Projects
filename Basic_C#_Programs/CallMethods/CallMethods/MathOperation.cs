b552919ae920e93f3550247aec2bcc2467493fe2
MJ231 <jayymedeiros@gmail.com>
2021-07-10 22:43:37 -0700

    Calling Methods
    Coding Program.

--- Basic_C#_Programs/CallMethods/CallMethods/MathOperation.cs
+++ Basic_C#_Programs/CallMethods/CallMethods/MathOperation.cs
@@ -8,17 +8,17 @@ namespace CallMethods
 {
     class MathOperation //Create class
     {
-        public static int Add(int numOne)
+        public static int Add (int numOne)
         {
-            return numOne + 100; //int Add1 = num1 + 100;
+            return numOne + 100; //int Add = numOne + 100;
         }
         public static int Mult (int numOne)
         {
-            return numOne * 40; //int Mult1 = num1 * 40;
+            return numOne * 40; //int Mult = numOne * 40;
         }
         public static int Subtract (int numOne)
         {
-            return numOne - 10; //int Subtract1 = num1 - 10;
+            return numOne - 10; //int Subtract = numOne - 10;
         }    
     }
 }
