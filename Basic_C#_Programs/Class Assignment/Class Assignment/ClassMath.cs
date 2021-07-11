ff0438d03331220dce99aed2f5c61d5c1cfa1798
MJ231 <jayymedeiros@gmail.com>
2021-07-10 23:32:38 -0700

    Class Program
    Coding Program.

---
+++ Basic_C#_Programs/Class Assignment/Class Assignment/ClassMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    public class ClassMath //Name class
+    {
+        public void MathMethod(int numOne, int numTwo) //Void method
+        {
+            int result = numOne / 2; //Results of operation 
+            int resultTwo = numTwo / 2;
+            Console.WriteLine("Your Number Divided by 2 = " + resultTwo);
+        }
+        public void MathMethod2(int numThree, int numFour) //More methods
+        {
+            int resultFive = numThree * numFour;
+            Console.WriteLine(numThree + " x " + numFour + " = " + resultFive);
+        }
+        public static int MathMethod2(int numOne, out int numTwo) //Output method
+        {
+            numTwo = numOne + 5;
+            return numTwo;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Class Assignment/Class Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Pick a Number of Your Choice."); //User input
+            int numTwo = Convert.ToInt32(Console.ReadLine()); //Convert integer
+            int numOne = 10; //Give integers
+            int car = 90;
+            int truck = 3;
+
+
+            ClassMath FirstOp = new ClassMath(); //Class method
+            FirstOp.MathMethod(numOne, numTwo);
+            FirstOp.MathMethod2(car, truck);
+
+            Console.ReadLine(); //End program
+        }
+    }
+}
