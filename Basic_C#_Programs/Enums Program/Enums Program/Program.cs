2ba31e92d5cb7796045209093e8019c316ff4517
MJ231 <jayymedeiros@gmail.com>
2021-07-12 16:52:32 -0700

    Enums Program
    Coding Program.

--- Basic_C#_Programs/Enums Program/Enums Program/Program.cs
+++ Basic_C#_Programs/Enums Program/Enums Program/Program.cs
@@ -10,25 +10,28 @@ namespace Enums_Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter the Current Day of the Week."); // User input
+            var entry = Console.ReadLine(); // Variable entry
+
+            Start:
             try // Try program to see if it works
             {
-                Console.WriteLine("Enter the Current Day of the Week."); // User input
-                string userDay = Console.ReadLine();
-
-                if (Enum.TryParse(userDay, out DaysOfTheWeek dayNum)) // If statement
-                {
-                    Console.WriteLine("Today is " + dayNum); // Ouput message
-                }
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), entry);
             }
-            catch (Exception ex) // Catch statement
+            catch (System.ArgumentException) // Catch statement
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please Enter an Actual Day of the Week."); // Error message to wrong entry
+                entry = Console.ReadLine();
+                goto Start;
             }
+            DaysOfTheWeek value = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), entry);
+
+            Console.WriteLine(value); // Vaule of variables
             Console.ReadLine(); // End program
         }
         public enum DaysOfTheWeek // Enum values for program
         {
-            Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7 // Days of the week as integers
+            Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7 // Days of the week as integers and variables
         }
     }
 }
