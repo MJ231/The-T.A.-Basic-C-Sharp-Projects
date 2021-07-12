91cc564032d820b37c6cbe0d1963f70fe8cd4dfd
MJ231 <jayymedeiros@gmail.com>
2021-07-11 22:43:00 -0700

    Generics Program
    Coding Program.

---
+++ Basic_C#_Programs/Generics/Generics/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> // Name class
+    {
+        public string firstName { get; set; } // Name properties for generics program
+        public string lastName { get; set; }
+        public int employeeID { get; set; }
+        public List<T> things { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/Generics/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> empList = new Employee<string>(); // Integrate employee class
+            Employee<int> empInt = new Employee<int>();
+            empList.things = new List<string>(); // Create loop to show on console application 
+            empList.things.Add("Albert");
+            empList.things.Add("Lopez");
+            empInt.things = new List<int>();
+            empInt.things.Add(30);
+            empInt.things.Add(40);
+
+            foreach (string item in empList.things) // Loop initiated
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in empInt.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine(); // End program
+        }
+    }
+}
