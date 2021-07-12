509fd5fd6d70d2242deb4f9376bb09e6114af7c3
MJ231 <jayymedeiros@gmail.com>
2021-07-12 02:34:05 -0700

    Date and Time Program
    Coding Program.

---
+++ Basic_C#_Programs/Date and Time Assignment/Date and Time Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Date_and_Time_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now; // Create application
+            Console.WriteLine("Today = " + DateTime.Now); // Shows the current date
+
+            Console.WriteLine("Pick a Number:"); // User input
+            int answer = Convert.ToInt32(Console.ReadLine()); // Convert to whole integers
+
+            TimeSpan duration = new TimeSpan(0, answer, 0, 0); // Integrate future time
+            DateTime futureTime = today.Add(duration);
+            Console.WriteLine("In " + answer + " Hours, it Will be: " + futureTime); // Displays future time per user input
+            Console.ReadLine(); // End program
+        }
+    }
+}
