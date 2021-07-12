28441d68b926de315009476ac52bbf60ea2cdfb1
MJ231 <jayymedeiros@gmail.com>
2021-07-12 03:10:24 -0700

    Constructor Assignment
    Coding Program.

---
+++ Basic_C#_Programs/Constructor/Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MovieTitle = "Black Widow"; // Constructor of movie title
+            var PaidSeat = "Row 3, Seat 10"; // Variable of paid seat 
+            Console.WriteLine("Enjoy Today's Feature Film: {0}", MovieTitle); // Display message on console application
+            Console.WriteLine("You Seat is " + PaidSeat); // Display message on console application
+            Console.ReadLine(); // End program
+        }
+    }
+}
---
+++ Basic_C#_Programs/Constructor/Constructor/SeatChart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class SeatChart // Class name
+    {
+        public SeatChart(string PaidSeat) : this(PaidSeat, 10) // Paid seat for movie goer
+        {
+
+        }
+        public SeatChart(string PaidSeat, int Payment) // Payment of seat
+        {
+            string CustSeating = PaidSeat; // String of customer's seat
+            int Price = Payment;
+        }
+    }
+}
