600b4a45a87ac727baca17e95256d7e30e63e56f
MJ231 <jayymedeiros@gmail.com>
2021-07-11 23:40:40 -0700

    Struct Assignment
    Coding Program.

---
+++ Basic_C#_Programs/Struct Program/Struct Program/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Program
+{
+    public struct Number // Create a struct
+    {
+        public decimal Amount; // Data type decimal amount
+    }
+}
---
+++ Basic_C#_Programs/Struct Program/Struct Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number s; // Assign amount
+            s.Amount = 10;
+
+            Console.WriteLine(s.Amount); // Enters amount on console application
+            Console.ReadLine(); // End program
+        }
+    }
+}
