b994117c8a947d30484bb466ef4da8c179dfd10b
MJ231 <jayymedeiros@gmail.com>
2021-07-12 02:22:18 -0700

    Logging Program
    Coding Program.

---
+++ Basic_C#_Programs/Logging/Logging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\doubl\logs\log.txt";
+            Console.WriteLine("Type in a Number:");
+            string inputNum = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                file.WriteLine(inputNum);
+            }
+            using (StreamReader file = new StreamReader(filePath, true))
+            {
+                string returnText = file.ReadToEnd().ToString();
+                Console.WriteLine(returnText);
+            }
+            Console.ReadLine();
+        }
+    }
+}
