f222a609914ec371bbf7ce30f55f84de005b3c51
MJ231 <jayymedeiros@gmail.com>
2021-07-13 01:11:00 -0700

    Class Program
    Coding Program.

---
+++ Basic_C#_Programs/Class Assignment/Class Assignment/StaticClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Class_Assignment
+{
+    static class StaticClass // Declare a class to be static
+    {
+    
+    
+    }
+}
